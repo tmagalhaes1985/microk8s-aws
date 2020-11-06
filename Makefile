c4ef47d4a7f403b0e13e72e18b50003f23d8fbf6
Thiago Magalhaes <thiago.magalhaes@flugel.it>
2020-11-06 18:48:46 -0300

    Add sudo to status and add $USER to microk8s group

--- Makefile
+++ Makefile
@@ -1,9 +1,8 @@
+PACKER_VERSION=1.6.5
 AMI_NAME_PREFIX?=flugel-microk8s-aws
 
 install_packer:
-	./bin/installdep.sh packer https://releases.hashicorp.com/packer/1.4.0/packer_1.4.0_linux_amd64.zip ./bin
+	./bin/installdep.sh packer https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip ./bin
 
 ami: install_packer
 	AMI_NAME_PREFIX=${AMI_NAME_PREFIX} packer build packer/microk8s-aws.json
-
-
--- packer/install-microk8s.sh
+++ packer/install-microk8s.sh
@@ -2,13 +2,15 @@
 
 set -e
 
-sudo apt-get install snapd
+sudo apt-get install -y snapd
 sudo snap remove kubectl-eks
 sudo snap install --classic --channel=1.17/stable microk8s
 sudo microk8s.start
 # Wait for microk8s to start
-until /snap/bin/microk8s.status ; do sleep 1 ; done
+until sudo /snap/bin/microk8s.status ; do sleep 1 ; done
 # Enable standard modules
 sudo microk8s.enable dns
+
+sudo usermod -aG microk8s $USER
 
 change-microk8s-password mytestcluster
