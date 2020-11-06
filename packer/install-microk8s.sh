98eb36ad17061909c24c8575266eec758041858c
Thiago Magalhaes <thiago.magalhaes@flugel.it>
2020-11-06 01:59:43 -0300

    Typo in interpreter / update version to 1.17

--- packer/install-microk8s.sh
+++ packer/install-microk8s.sh
@@ -1,10 +1,10 @@
-#/!bin/bash
+#!/bin/bash
 
 set -e
 
 sudo apt-get install snapd
 sudo snap remove kubectl-eks
-sudo snap install --classic --channel=1.15/stable microk8s
+sudo snap install --classic --channel=1.17/stable microk8s
 sudo microk8s.start
 # Wait for microk8s to start
 until /snap/bin/microk8s.status ; do sleep 1 ; done
